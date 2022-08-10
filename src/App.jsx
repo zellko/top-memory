9cc462b5208629e793bcae065e175d9d7bd89baf
zellko <wyttenda@tutanota.com>
2022-08-10 23:07:44 +0200

    Setup React Project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './styles/App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Memory Card</h1>
+    </div>
+  );
+}
+
+export default App;
