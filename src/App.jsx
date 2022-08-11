3eb447f43d05b921f50b739843f32d4d86bb26e8
zellko <wyttenda@tutanota.com>
2022-08-11 19:39:14 +0200

    Added 'Card shuffling' and game logic
    Modified App.jsx:
    - Added Cards (cardArray) random shuffling
    - Added shuffling each time card is being clicked
    - Added game logic:
    	- Check if a card is cliked more than once (loose).
    	- Check if all cards where clicked (win).

--- src/App.jsx
+++ src/App.jsx
@@ -2,56 +2,99 @@ import './styles/App.css';
 import React, { useState, useEffect } from 'react';
 import Board from './components/Board';
 
+let clickedCards = [];
+let onFirstLoad = true;
+
+function shuffleArray(array, shuffledArray = []) {
+  // Shuffle cardArray in a random order...
+  // ... to render the cards at random  position on the screen
+  if (array.length === 0) return shuffledArray;
+
+  const randomNumber = Math.floor((Math.random() * array.length));
+
+  shuffledArray.push(array[randomNumber]);
+  array.splice(randomNumber, 1);
+
+  shuffleArray(array, shuffledArray);
+
+  return shuffledArray;
+}
+
+function checkIfUserWon() {
+  if (clickedCards.length === 12) {
+    console.log('You Won !');
+    clickedCards = [];
+    return true;
+  }
+  return false;
+}
+
+function checkIfUserLoose(cardid) {
+  if (clickedCards.includes(cardid)) {
+    console.log('You Loose !');
+    clickedCards = [];
+    return true;
+  }
+  return false;
+}
+
 function App() {
   const [cardArray, setCardArray] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
 
-  function shuffleArray() {
-    // Shuffle cardArray in a random order...
-    // ... to render the cards at random  position on the screen
+  useEffect(() => {
+    if (onFirstLoad) {
+      onFirstLoad = false;
 
-    // TEST: For now, push first array element to the end.
-    const cardArrayCopy = [...cardArray];
-    const firstElement = cardArrayCopy.shift();
-    cardArrayCopy.push(firstElement);
+      // Shuffles the cards...
+      const shuffledArray = shuffleArray(cardArray);
+      setCardArray(shuffledArray);
+    }
+  });
 
-    setCardArray(cardArrayCopy);
-  }
+  useEffect(() => {
+    // Shuffles the cards...
 
-  function checkIfUserWon() {
+    const onCardClick = (e) => {
+      // Each time user click on a card...
 
-  }
+      // Get the card ID...
+      const clickedCardId = e.target.parentElement.getAttribute('cardid');
+      console.log(clickedCardId);
 
-  function checkIfUserLoose() {
+      // Check if user loose (he clicked on a card more than once)
+      const isGameOver = checkIfUserLoose(clickedCardId);
+      if (!isGameOver) {
+        // If user did not loose...
 
-  }
+        // Add the card to the clickedCard array
+        clickedCards.push(clickedCardId);
 
-  useEffect(() => () => {
+        // Check if user won (he clicked on all the 12 cards,
+        //    without clicking on a single card more than once)
+        checkIfUserWon();
+      }
+
+      // Shuffles the cards...
+      const shuffledArray = shuffleArray(cardArray);
+      setCardArray(shuffledArray);
+
+      console.log(clickedCards);
+    };
+
     const cards = document.querySelectorAll('.card');
 
-    cards.forEach((card) => card.addEventListener('click', (e) => {
-      // When a card is clicked...
-      // Get the card id...
-      const clickedCardId = card.getAttribute('cardid');
-      console.log(clickedCardId);
-      // Check if user clicked all the cards.
-
-      // Check if card was already clicked by user.
-
-      // Stop propagation to only record one click
-      e.stopImmediatePropagation();
-    }));
+    cards.forEach((card) => card.addEventListener('click', onCardClick));
 
     return () => {
-      cards.forEach((card) => card.removeEventListener('click', () => {}));
+      cards.forEach((card) => card.removeEventListener('click', onCardClick));
     };
-  });
+  }, [cardArray]);
 
   return (
     <div className="App">
       <h1>ToDo Header</h1>
       <h2>ToDo Score</h2>
       <Board cardArray={cardArray} />
-      <button type="button" onClick={shuffleArray}>Shuffle</button>
     </div>
   );
 }
